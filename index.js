98c8eeecd4d6b33b7ea6a4c17cb55634739689c1
Rasmus Lind <rasmuslind@Rasmuss-MacBook-Air.local>
2021-10-30 16:17:09 +0200

    added comments

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-document.getElementById("button1").addEventListener("click", () => {
+//OPG 2A
+//Jeg laver en eventlistener som bruger table og button1 id og button1 gemmer den pga display=none og table=block gør at vi vil se table
+addEventListener("click", () => {
     document.getElementById("button1").style.display="none"; 
     document.getElementById("table").style.display="block"; 
 })
 
+//Jeg indsætter dataerne som blev brugt i OPG 1c
 let besætning = [
     { kategori: "Køer", antal: 50 },
     { kategori: "Hunde", antal: 1 },
@@ -10,6 +13,7 @@ let besætning = [
     { kategori: "Får", antal: 20 },
 ];
 
+//Herefter laver jeg en funktion som looper igennem besætning array og laver det om til et table
 function buildTable (data){
     var table = document.getElementById("table")
 
@@ -23,6 +27,7 @@ function buildTable (data){
     }
 }
 
+//til sidst kalder jeg funktionen med besætning array i og der laves dermed et table ud af dette array
 buildTable(besætning)
 
 
